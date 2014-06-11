d8708a640e19bd2d8ade9993bac1ed5ec35e7434
jeffrymorris <jeffrymorris@gmail.com>
2014-06-11 16:19:04 -0700

    initial

---
+++ cb-client/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Couchbase;
+using Couchbase.Configuration;
+using Enyim;
+using Enyim.Caching.Memcached;
+
+namespace cb_client
+{
+    class Program
+    {
+        public static List<CouchbaseClient> Clients = new List<CouchbaseClient>(); 
+        static void Main(string[] args)
+        {
+            var config1 = new CouchbaseClientConfiguration { Bucket = "default" };
+            config1.Urls.Add(new Uri("http://localhost:8091/pools"));
+            
+            var client1 = new CouchbaseClient(config1);
+            Clients.Add(client1);
+
+            Thread.Sleep(100);
+            var config2 = new CouchbaseClientConfiguration { Bucket = "default" };
+            config2.Urls.Add(new Uri("http://localhost:8091/pools"));
+
+            var client2 = new CouchbaseClient(config2);
+            Clients.Add(client2);
+
+            const int itemsToProcess = 100000;
+            foreach (var client in Clients)
+            {
+               //ProcessWithTap(client, itemsToProcess/Clients.Count);
+                ProcessWithParallel(client, itemsToProcess / Clients.Count);
+            }
+
+            Console.Read();
+        }
+
+        static void ProcessSync(CouchbaseClient client, int numberOfItems)
+        {
+            for (var i = 0; i < numberOfItems; i++)
+            {
+                PerformGetSet(client, i);
+            }
+        }
+
+        static void ProcessWithTap(CouchbaseClient client, int numberOfItems)
+        {
+            for (var i = 0; i < numberOfItems; i++)
+            {
+                var i1 = i;
+                Task.Run(() => PerformGetSet(client, i1));
+            }
+        }
+
+        static void ProcessWithParallel(CouchbaseClient client, int numberOfItems)
+        {
+            var options = new ParallelOptions { MaxDegreeOfParallelism = 4 };
+
+            Parallel.For(0, numberOfItems, options, i => PerformGetSet(client, i));
+        }
+
+        private static void PerformGetSet(CouchbaseClient client, int i)
+        {
+            var key = string.Format("key{0}", i);
+            var document = string.Concat("{\"value\":\"", i, "\"}");
+                        
+            var set = client.ExecuteStore(StoreMode.Set, key, document);
+            Console.WriteLine("Set: {0} - {1} - {2}", key, set.Success, Thread.CurrentThread.ManagedThreadId);
+
+            var get = client.ExecuteGet<dynamic>(key);
+            Console.WriteLine("Get: {0} - {1} - {2}", key, get.Success, Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ cb-client2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Couchbase;
+using Couchbase.Configuration;
+using Enyim.Caching.Memcached;
+
+namespace cb_client2
+{
+    class Program
+    {
+        private static List<Thread> threads = new List<Thread>(); 
+        static void Main(string[] args)
+        {
+            var numberOfItems = 100000;
+            var threadCount = 10;
+            for (int i = 0; i < threadCount; i++)
+            {
+                var worker = new Worker {NumberOfItems = numberOfItems/threadCount};
+                var thread = new Thread(worker.Process); 
+                thread.Start();
+                Thread.Sleep(10);//slight delay
+            }
+            Console.Read();
+        }
+
+
+        internal sealed class Worker
+        {
+            public Worker()
+            {
+                var config = new CouchbaseClientConfiguration { Bucket = "default" };
+                config.Urls.Add(new Uri("http://localhost:8091/pools"));
+                _client = new CouchbaseClient(config);
+            }
+
+            private readonly CouchbaseClient _client;
+            public int NumberOfItems;
+
+            public void Process()
+            {
+                for (int i = 0; i < NumberOfItems; i++)
+                {
+                    PerformGetSet(_client, i);
+                }
+            }
+
+            private static void PerformGetSet(CouchbaseClient client, int i)
+            {
+                var key = string.Format("key{0}", i);
+                var document = string.Concat("{\"value\":\"", i, "\"}");
+
+                var set = client.ExecuteStore(StoreMode.Set, key, document);
+                Console.WriteLine("Set: {0} - {1} - {2}", key, set.Success, Thread.CurrentThread.ManagedThreadId);
+
+                var get = client.ExecuteGet<dynamic>(key);
+                Console.WriteLine("Get: {0} - {1} - {2}", key, get.Success, Thread.CurrentThread.ManagedThreadId);
+            }
+        }
+    }
+}
